3c86f02ca44c897e723c1e6656a438c970c5d4e4
yammyam <tkdxo999@gmail.com>
2025-06-18 04:08:00 +0900

    레이아웃과 라우트그룹

---
+++ src/app/(with-searchbar)/search/setting/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div>세팅페이지</div>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -19,7 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} `}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
